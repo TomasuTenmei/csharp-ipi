aa65796c48088b783c6d710c12e65c47a4c91c1d
Tomasu <Thomas.ARNAUD.DEV@outlook.com>
2023-11-21 13:42:37 +0100

    suite TP C#

---
+++ Exercice24/Program.cs
@@ -0,0 +1,69 @@
+﻿class Program
+{
+    static void inverseListe(List<string> liste)
+    {
+        List<string> listeInverse = new List<string>();
+
+        for (int i = liste.Count - 1; i >= 0; i--)
+        {
+            listeInverse.Add(liste[i]);
+        }
+
+        System.Console.WriteLine("\nListe inversée :");
+
+        for (int i = 0; i < listeInverse.Count; i++)
+        {
+            System.Console.WriteLine(listeInverse[i]);
+        }
+    }
+
+    static void printListe(List<string> liste)
+    {
+        System.Console.WriteLine("\nListe des noms :");
+
+        for (int i = 0; i < liste.Count; i++)
+        {
+            System.Console.WriteLine(liste[i]);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var name = "";
+        List<string> nameList = new List<string>();
+
+        while (true)
+        {
+            System.Console.Write("Entrez un nom n° " + (nameList.Count + 1) + " : ");
+            name = Console.ReadLine();
+
+            if (int.TryParse(name, out int n))
+            {
+                if (nameList.Count <= 5)
+                {
+                    System.Console.WriteLine("Vous ne pouvez pas quittez le programme avant d'avoir entré 6 noms.");
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            else
+            {
+                nameList.Add(name);
+            }
+
+        }
+
+        printListe(nameList);
+
+        nameList.RemoveAt(1);
+        nameList.RemoveAt(3);
+        nameList[2] = "Toto";
+
+        printListe(nameList);
+
+        inverseListe(nameList);
+    }
+}
---
+++ Exercice25/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> hashSet = new HashSet<int>();
+
+        for (int i = 0; i < 1000000; i++)
+        {
+            hashSet.Add(i);
+        }
+
+        List<int> list = new List<int>();
+
+        for (int i = 0; i < 1000000; i++)
+        {
+            list.Add(i);
+        }
+        
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+        sw.Start();
+        System.Console.WriteLine(list.Contains(900000));
+        sw.Stop();
+        System.Console.WriteLine("Liste : " + sw.ElapsedMilliseconds + " ms");
+        sw.Reset();
+
+        sw.Start();
+        System.Console.WriteLine(hashSet.Contains(900000));
+        sw.Stop();
+        System.Console.WriteLine("HashSet : " + sw.ElapsedMilliseconds + " ms");
+        sw.Reset();
+    }
+}
